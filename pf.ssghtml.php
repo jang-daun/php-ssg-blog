a263fb542cde0b80fc759f546c7b8f7eb955be6e
jang-daun <moone0327@gmail.com>
2021-01-15 17:02:26 +0900

    command

---
+++ pf.ssghtml.php
@@ -0,0 +1,18 @@
+<?php
+require_once "head.php";
+?>
+
+<link rel="stylesheet" href="css/about.css">
+<script src="js/about.js" defer></script>
+
+<section class="section-title con-min-width">
+  <h1 class="con">
+    <span>
+      PORFOLIO
+    </span>
+  </h1>
+</section>
+
+<?php
+require_once "foot.php";
+?>
