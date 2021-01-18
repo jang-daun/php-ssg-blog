5d225332cff4975f866cf04b2c958c8e2de82e1a
jang-daun <moone0327@gmail.com>
2021-01-18 22:22:50 +0900

    댓글 기능 수정

--- article_detail.php
+++ article_detail.php
@@ -43,7 +43,7 @@
         };
         (function() { // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
-        s.src = 'https://phpblog-2.disqus.com/embed.js';
+        s.src = 'https://down-2.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
         })();
