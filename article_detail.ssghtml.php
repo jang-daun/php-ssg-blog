22bf9f7507bd85d4cc7d251c11221046f22b970b
jang-daun <moone0327@gmail.com>
2021-01-26 16:48:04 +0900

    변형

--- article_detail.ssghtml.php
+++ article_detail.ssghtml.php
@@ -20,21 +20,21 @@ $selectedArticle = &getArticleById($articleId);
 
 <section class="section-article-detail padding-0-10 con-min-width">
     <div class="con">
-        <h1 class="article-list-box__title"><a href="article_detail_1.ssghtml.php"><?=$selectedArticle["title"]?></a></h1>
-        
+        <h1 class="article-list-box__title"><?=$selectedArticle["title"]?></h1>
+
         <div class="article-list-box__reg-date"><?=$selectedArticle["regDate"]?></div>
         
         <div class="article-list-box__writer">
             <span><?=$selectedArticle["writerName"]?></span>
-            <span>
-                <?=$selectedArticle["writerAvatar"]?>
-            </span>
+            <span><?=$selectedArticle["writerAvatar"]?></span>
+        </div>
+
+        <div class="article-list-box__tags">
+            <?=getArticleTagsHtml($selectedArticle["id"])?>
         </div>
         
         <div class="article-list-box__body">
-            <script type="text/x-template">
-                <?=$selectedArticle["body"]?>
-            </script>
+            <script type="text/x-template"><?=$selectedArticle['body']?></script>
             <div class="toast-ui-viewer"></div>
         </div>
     </div>
