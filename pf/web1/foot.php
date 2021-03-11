38b8ba98149caedefeffc1e644258fde40cb6eff
jang-daun <moone0327@gmail.com>
2021-03-12 01:55:14 +0900

    페이지

---
+++ pf/web1/foot.php
@@ -0,0 +1,10 @@
+ <!--footer 시작-->
+ <div class="section-13 con-min-width width-100p">
+        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/footer.svg" alt="">
+    </div>
+    <!--footer 끝-->
+
+
+
+</body>
+</html>
