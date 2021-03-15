cad5524666e9531d9f4a863004f8ea877dd3c1b3
jang-daun <moone0327@gmail.com>
2021-03-15 16:21:58 +0900

    .

---
+++ pf/web2/foot.php
@@ -0,0 +1,10 @@
+ <!--footer 시작-->
+ <div class="section-13 con-min-width width-100p">
+        
+    </div>
+    <!--footer 끝-->
+
+
+
+</body>
+</html>
---
+++ pf/web2/head.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>그린피스 - <?=$title?></title>
+
+    <!--폰트 시작-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
+    <!--폰트 끝-->
+
+    <!--폰트어썸 시작-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <!--폰트어썸 끝-->
+
+    <!--제이쿼리 시작-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!--제이쿼리 끝-->
+
+    <!--스와이퍼 시작-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <!--스와이퍼 끝-->
+
+    <!--gsap 시작-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="common.css">
+    <script src="common.js" defer></script>
+
+</head>
+
+<body>
+    <!--header시작-->
+    <div class="section-1 con-min-width">
+        <div class="con menu-box">
+            <a href="index.ssghtml.php" class="logo">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/greenlogo.svg" alt="">
+                <span><i class="fas fa-sort-down"></i></span>
+                <span>East Aisa</span>
+            </a>
+            <nav class="menu-box__menu-bar flex flex-j-c">
+                <ul class="flex">
+                    <li>
+                        <a href="#">활동</a>
+                        <ul>
+                            <li><a href="#">그린 캠페인</a></li>
+                            <li><a href="#">그린 프로젝트</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">참여하기</a>
+                        <ul>
+                            <li><a href="#">서명하기</a></li>
+                            <li><a href="#">챌린지</a></li>
+                            <li><a href="#">후원하기</a></li>
+                            <li><a href="#">활동 동참하기</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">소식</a>
+                        <ul>
+                            <li><a href="#">최신 뉴스</a></li>
+                            <li><a href="#">서포터 소식</a></li>
+                            <li><a href="#">with 뉴닉</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">자료</a>
+                        <ul>
+                            <li><a href="#">보고서</a></li>
+                            <li><a href="#">보도 자료</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">소개</a>
+                        <ul>
+                            <li><a href="#">개요</a></li>
+                            <li><a href="#">미션</a></li>
+                            <li><a href="#">행동방식</a></li>
+                            <li><a href="#">사람들</a></li>
+                            <li><a href="#">인재 채용</a></li>
+                            <li><a href="#">연락처</a></li>
+                            <li><a href="#">자주하는 질문</a></li>
+                            <li><a href="#">SNS</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div class="more-box flex flex-j-c">
+                <span><i class="fas fa-search"></i></span>
+                <span>후원하기</span>
+                <span>로그인</span>
+            </div>
+        </div>
+    </div>
+    <!--header끝-->
