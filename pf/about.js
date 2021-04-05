1024e485822b897f5375bf29f9d5437a653761c8
jang-daun <moone0327@gmail.com>
2021-04-05 23:59:48 +0900

    .

--- pf/about.js
+++ pf/about.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
     },
   });
--- pf/about.ssghtml.php
+++ pf/about.ssghtml.php
@@ -13,18 +13,47 @@ require_once "head.php";
 
 <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">
-          <div class="orbit">
-              
-          </div>
-      </div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
+        <div class="swiper-slide">
+            <div class="contain">
+                <div class="orbit">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/no1.svg" alt="">
+                </div>
+                <div class="slogan">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/aboutslogan.svg" alt="">
+                </div>
+                <div class="content">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/aboutcontent.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="contain">
+                <div class="orbit">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/no2.svg" alt="">
+                </div>
+                <div class="title">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/tt.svg" alt="">
+                </div>
+                <div class="pfcontent">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/profilecontent.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="contain">
+                <div class="orbit">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/no3.svg" alt="">
+                </div>
+                <div class="ability">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/ability.svg" alt="">
+                    <img src="https://cdn.jsdelivr.net/gh/jang-daun/portfolio/ability-c.svg" alt="">
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- Add Arrows -->
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-  </div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination"></div>
+</div>
 
 <?php
 require_once "foot.php";
