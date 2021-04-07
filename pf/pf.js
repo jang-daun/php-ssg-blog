ef0cb4ad4f29f4bc085ad6d0fbb96d6c1d2463a8
jang-daun <moone0327@gmail.com>
2021-04-08 00:38:45 +0900

    .

---
+++ pf/pf.js
@@ -0,0 +1,19 @@
+
+
+console.clear();
+
+function Fullpage__init() {
+  var myFullpage = new fullpage('#fullpage', {
+    // verticalCentered:false, // 수직중앙정렬
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['no.1 - web', 'no.2 - web', 'no.3 - UI|UX'],
+    responsiveWidth: 1100,
+    afterResponsive: function(isResponsive){
+      console.log(isResponsive);
+    }
+  });
+}
+
+Fullpage__init();
--- pf/pf.ssghtml.php
+++ pf/pf.ssghtml.php
@@ -3,8 +3,29 @@ $pageCode = 'portfolio';
 $pageTitle = "PORTFOLIO";
 require_once "head.php";
 ?>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.9/fullpage.min.css">
+
 <link rel="stylesheet" href="pf.css">
 <script src="pf.js" defer></script>
+
+
+<div id="fullpage">
+  <section class="section section1 con-min-width bg-red fp-auto-height-responsive">
+    <div class="backcircle"></div>
+  </section>
+  <section class="section section1 con-min-width bg-black">
+    <div class="backcircle backcircle2"></div>
+  </section>
+  <section class="section section1 con-min-width bg-white">
+    <div class="backcircle backcircle3"></div>
+  </section>
+  </section>
+</div>
+
+
 
 <?php
 require_once "foot.php";
