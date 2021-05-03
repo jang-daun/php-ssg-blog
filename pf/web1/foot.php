54581142032fde91902f8054190985587c9de316
jang-daun <moone0327@gmail.com>
2021-05-03 19:09:09 +0900

    width 수정

--- pf/web1/foot.php
+++ pf/web1/foot.php
@@ -1,6 +1,8 @@
  <!--footer 시작-->
  <div class="section-13 con-min-width width-100p">
-        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/footer.svg" alt="">
+        <div class="con">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/footer.svg" alt="">
+        </div>
     </div>
     <!--footer 끝-->
 
