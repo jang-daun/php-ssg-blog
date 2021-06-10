04191f1a2ac81a7d3a02c5bc636f5a09bc8b5d45
jang-daun <moone0327@gmail.com>
2021-06-11 00:43:00 +0900

    .

---
+++ pf/web3/index.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+  });
+
+  //tab
+  var tabBtn = $(".tab-btn > ul > li");
+var tabCont = $(".tab-cont > div");
+
+tabCont.hide().eq(0).show();
+
+tabBtn.click(function(e){
+    e.preventDefault();
+    var target = $(this);
+    var index = target.index();
+    tabBtn.removeClass("active");
+    target.addClass("active");
+    tabCont.css("display", "none");
+    tabCont.eq(index).css("display", "block");
+});
