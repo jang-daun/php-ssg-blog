a12d616bdb4444d7bb223eb3f00f9e519b474799
jang-daun <moone0327@gmail.com>
2021-08-20 16:49:15 +0900

    .

--- pf/web4/common.js
+++ pf/web4/common.js
@@ -12,3 +12,22 @@ $('.gnb__menu-bar_nav nav > ul > li').click(function(){
   $('.gnb__menu-bar_nav > ul > li').mouseleave(function(){
     $('.gnb__menu').attr('data-sel-index', '');
   });
+
+
+
+
+  var tabBtn = $(".main-bord_btn > .btn");
+  var tabCont = $(".main-bord_contents");
+  
+  tabCont.hide().eq(0).show();
+  
+  tabBtn.click(function(e){
+      e.preventDefault();
+      var target = $(this);
+      var index = target.index();
+      tabBtn.removeClass("active");
+      target.addClass("active");
+      tabCont.css("display", "none");
+      tabCont.eq(index).css("display", "flex");
+  });
+  
