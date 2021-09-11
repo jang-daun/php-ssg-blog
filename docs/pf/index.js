f746aaed1a7a8a0a7805a2c22e242dadedcf678a
jang-daun <moone0327@gmail.com>
2021-09-12 02:00:32 +0900

    .

--- docs/pf/index.js
+++ docs/pf/index.js
@@ -1,13 +1,14 @@
-function Circle1__init() {
-    const circleType = new CircleType($('.circle-6 > .txt').get(0));
-  
-    // Set the text radius and direction. Note: setter methods are chainable.
-    circleType.radius(parseInt($('.circle-6').width() / 2));
-    
-    setTimeout(function() {
-      $('.circle-6').addClass('active');
-    }, 1000);
-  }
-  
-  Circle1__init();
-  
+const $cursor = $('.cursor');
+const $cursorSahdow = $('.cursor-shadow');
+
+$(window).mousemove(function (e) {
+    $cursor.css({
+        top: e.clientY,
+        left: e.clientX
+    });
+
+    $cursorSahdow.css({
+        top: e.clientY,
+        left: e.clientX
+    });
+});
--- pf/ex.ssghtml.php
+++ pf/ex.ssghtml.php
@@ -1,10 +1,118 @@
 <?php
 $pageCode = 'experience';
-$pageTitle = "EXPERIENCE";
+$pageTitle = "experience";
 require_once "head.php";
 ?>
 <link rel="stylesheet" href="ex.css">
 <script src="ex.js" defer></script>
+
+
+
+<div class="etc__intro con-min-width">
+    <div class="con">
+        <img class="title" src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/etc_what.svg">
+        <div>디자인 위한 다양한 경험들</div>
+        <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">
+    </div>
+</div>
+
+<div class="etc__section about__youtube con-min-width">
+    <div class="con flex">
+        <div class="etc_img">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/etc_youtube.png">
+        </div>
+        <div class="etc_shape">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">   
+        </div>
+        <div class="etc_txt flex">
+            <span>Youtube<br>Study-log</span>
+            <span>코딩 & 디자인 작업 기록</span>
+            <a class="view flex" href="https://www.youtube.com/channel/UCFuSljPH66Z-8sn6PNtfBCA/videos">
+                <span>view</span>
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/arrowblue.svg">   
+            </a>
+        </div>
+    </div>
+</div>
+
+
+<div class="etc__section about__t-story con-min-width">
+    <div class="con flex">
+        <div class="etc_txt flex">
+            <span>Design<br>t-story</span>
+            <span>기타 디자인 작업 업로드</span>
+            <a class="view view-yellow flex" href="https://www.youtube.com/channel/UCFuSljPH66Z-8sn6PNtfBCA/videos">
+                <span>view</span>
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/arrowyellow.svg">   
+            </a>
+        </div>
+        <div class="etc_shape">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">   
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">   
+        </div>
+        <div class="etc_img">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/etc_t-story.png">
+        </div>
+    </div>
+</div>
+
+
+<div class="etc__section about__subway con-min-width">
+    <div class="con flex">
+        <div class="etc_img">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/etc_subway.png">
+        </div>
+        <div class="etc_shape">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">   
+        </div>
+        <div class="etc_txt flex">
+            <span>Web<br>Coding</span>
+            <span>서브웨이 모작 코딩</span>
+            <a class="view view-green flex" href="https://www.youtube.com/channel/UCFuSljPH66Z-8sn6PNtfBCA/videos">
+                <span>view</span>
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/arrowgreen.svg">   
+            </a>
+        </div>
+    </div>
+</div>
+
+
+<div class="etc__section about__calender con-min-width">
+    <div class="con flex">
+        <div class="etc_txt flex">
+            <span>2021<br>Calender</span>
+            <span>개인 작업 </span>
+            <a class="view view-orange flex" href="https://www.youtube.com/channel/UCFuSljPH66Z-8sn6PNtfBCA/videos">
+                <span>view</span>
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/arroworange.svg">   
+            </a>
+        </div>
+        <div class="etc_shape">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">  
+        </div>
+        <div class="etc_img">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/etc_calender.png">
+        </div>
+    </div>
+</div>
+
+
+<div class="etc__section about__youtube con-min-width">
+    <div class="con flex">
+        <div class="etc_img">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_sticker.png">
+        </div>
+        <div class="etc_shape">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/shape2.svg">   
+        </div>
+        <div class="etc_txt flex">
+            <span>Object<br>Sticker</span>
+            <span>개인 작업</span>
+        </div>
+    </div>
+</div>
+
+
 
 <?php
 require_once "foot.php";
