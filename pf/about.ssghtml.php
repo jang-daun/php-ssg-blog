6ce45a52bde558ce968339ec86e4df83a839725c
jang-daun <moone0327@gmail.com>
2021-09-11 18:43:34 +0900

    about

--- pf/about.ssghtml.php
+++ pf/about.ssghtml.php
@@ -34,6 +34,102 @@ require_once "head.php";
 </div>
 
 
+<div class="about__section about__mind con-min-width">
+    <img class="tape" src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_tape.png">
+    <div class="con">
+        <div class="about_title">
+            <span>01</span>
+            <span>Mind</span>
+        </div>
+        <div class="about_content">
+            <span>디자인의 기본은 끊임없이 관찰하고 생각하는 것입니다.<br>
+                기본 속에서 탄생하는 아이디어가 더 큰 디자인의 세계 속으로 갈 수 있도록 합니다.<br>
+                <br>
+                디자인의 세계는 무궁무진합니다.<br>
+                그 무한한 세계에서 디자이너가, 사용자가, 클라이언트가 바라는 디자인을 하겠습니다.<br>
+                <br>
+                저는 긍정적이고 밝은 모습으로 사람들과 소통하며 상대방의 입장을 이해하고 헤아릴 수 있는 사람입니다.<br>
+                이러한 저의 모습을 바탕으로 끊임없는 사고를 통해 디자인 하겠습니다.<br>
+                <br>
+                끊임없이 관찰하고 생각하는 디자이너 ‘장다운’입니다.</span>
+        </div>
+    </div>
+</div>
+
+
+<div class="about__section about__mind-map con-min-width">
+    <div class="con flex">
+        <div class="about_title">
+            <span>02</span>
+            <span>Mind map</span>
+        </div>
+        <div class="about_content">
+            <div class="about_content__title">About me</div>
+            <div class="about_content__list_back">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_list_back.svg">
+            </div>
+            <div class="about_content__sub">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_mind-map.svg">
+            </div>
+            <div class="about_content__dotted-1">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/dotted-1.svg">
+            </div>
+            <div class="about_content__dotted-2">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/dotted-2.svg">
+            </div>
+            <div class="about_content__sub-cont1">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_txt-1.svg">
+            </div>
+            <div class="about_content__dotted-3">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/dotted-3.svg">
+            </div>
+            <div class="about_content__sub-cont2">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_txt-2.svg">
+            </div>
+            <div class="about_content__list">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_list.svg">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="about__section about__personal con-min-width">
+    <div class="con">
+        <div class="about_title">
+            <span>03</span>
+            <span>Personal ability</span>
+        </div>
+        <div class="about_content">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_personal.svg">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_personal_gage.svg">
+        </div>
+    </div>
+</div>
+
+
+<div class="about__section about__skill con-min-width">
+    <div class="con">
+        <div class="about_title">
+            <span>04</span>
+            <span>Skill</span>
+        </div>
+        <div class="about_content flex">
+            <div class="about_content__txt">
+                <div class="forty">믿을수 있는</div>
+                <div><span class="forty forty-no">디자이너</span> <span class="forty forty-no2">장다운</span></div>
+                <div class="twenty">다양한 프로그램을 사용할 수 있는<br>
+스킬을 보유하고 있습니다.</div>
+<div class="red hover-me">hover me!</div>
+            </div>
+            <div class="about_content__pic">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_me__label-1.png">
+                <img src="https://cdn.jsdelivr.net/gh/jang-daun/phpblog/portimg/about_me__label-2.png">
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 <?php
 require_once "foot.php";
