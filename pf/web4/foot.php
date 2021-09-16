b4cf49d0196dbcea14a72b77fd8c288104a099dc
jang-daun <moone0327@gmail.com>
2021-09-16 17:51:27 +0900

    .

--- pf/web4/foot.php
+++ pf/web4/foot.php
@@ -1,2 +1,23 @@
+<div class="footer flex con-min-width">
+    <div class="con flex">
+        <div class="logo-box flex">
+            <img src="https://cdn.jsdelivr.net/gh/jang-daun/fairytale/img/foot_logo.svg" alt="">
+        </div>
+        <div class="company-box">
+        [10229] 경기 고양시 일산서구 덕이동 1529 하이파크시티 일산파밀리에2단지 211동 501호<br>
+사업자등록번호 : 101-04-85275 | 통신판매번호 : 제18-02587호<br>
+Copyright 2002-2018 picturebook-illust.com All right reserved<br>
+<br>
+<b>개인정보 보호정책</b> | 회원탈퇴
+        </div>
+        <div class="personal-box">운영자 : 임해영<br>
+<br>
+근무시간 : PM 12시~ PM 4시<br>
+<br>
+이메일 : koreanillust@naver.com</div>
+    </div>
+</div>
+
+
 </body>
 </html>
