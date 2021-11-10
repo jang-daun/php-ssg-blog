c7a08810a84c414206550ec1a7d75ac51211767e
jang-daun <moone0327@gmail.com>
2021-11-11 00:29:24 +0900

    그린피스 캠페인

--- pf/greenpeace/index.js
+++ pf/greenpeace/index.js
@@ -1,8 +1,18 @@
-var swiper = new Swiper('.swiper', {
+var swiper = new Swiper('#main-visual .swiper', {
     spaceBetween: 30,
     effect: 'fade',
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
       },
+  });
+
+
+
+  var swiper = new Swiper('#main-content-2 .swiper', {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
--- pf/greenpeace/index.ssghtml.php
+++ pf/greenpeace/index.ssghtml.php
@@ -30,13 +30,176 @@ require_once "head.php";
             </div>
         </div>
     </div>
-    <div class="main-visual__txt">
-        <div class="main-visual__txt-title">
+    <div class="main-visual__txt flex">
+        <div class="main-visual__txt-title flex">
             <span>For nature For future</span>
             <span>그린피스는 더 푸르고 건강한 세상을 위해 싸움을 멈추지 않을 것입니다.</span>
         </div>
-        <div class="main-visual__txt-cont">
+        <div class="main-visual__txt-cont flex">
             <a href="#">동참 하기</a>
+        </div>
+    </div>
+</div>
+
+<div id="main-content-1">
+    <div class="con">
+        <div class="main-content-1__campaign">
+            <div class="campaign__cont flex">
+                <div class="cont__2box flex">
+                    <div class="campaign__title">
+                        <a class="titleeng" href="#">Green Campaign</a>
+                        <a class="titleko" href="#">그린피스 캠페인</a>
+                        <div class="greenbar"></div>
+                        <div class="content">그린피스에서 진행하는 지역 및 글로벌 캠페인에 대해 자세히 알아보세요.</div>
+                    </div>
+                </div>
+                <div class="cont__2box flex">
+                    <div class="box_4box">
+                        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_ocean.jpg"
+                            alt="">
+                        <div class="box_4box__whiteopacity"></div>
+                        <div class="box_4box__white"></div>
+                        <div class="box_4box__border"></div>
+                        <a class="box_4box__txt-box flex" href="#">
+                            <div class="flex txt-box__title">
+                                <span>해양</span>
+                                <span>Oceans</span>
+                            </div>
+                            <div class="campaign__cont__more flex">
+                                <div class="campaign__cont__more-box">
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="box_4box">
+                        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_health.jpg"
+                            alt="">
+                        <div class="box_4box__whiteopacity"></div>
+                        <div class="box_4box__white"></div>
+                        <div class="box_4box__border"></div>
+                        <a class="box_4box__txt-box flex" href="#">
+                            <div class="flex txt-box__title">
+                                <span>생활</span>
+                                <span>Health</span>
+                            </div>
+                            <div class="campaign__cont__more flex">
+                                <div class="campaign__cont__more-box">
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="campaign__cont flex">
+                <div class="cont__2box flex">
+                    <div class="box_4box">
+                        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_climate.jpg"
+                            alt="">
+                        <div class="box_4box__whiteopacity"></div>
+                        <div class="box_4box__white"></div>
+                        <div class="box_4box__border"></div>
+                        <a class="box_4box__txt-box flex" href="#">
+                            <div class="flex txt-box__title">
+                                <span>기후</span>
+                                <span>Climate</span>
+                            </div>
+                            <div class="campaign__cont__more flex">
+                                <div class="campaign__cont__more-box">
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="cont__2box flex">
+                        <div class="box_4box">
+                            <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_forest.jpg"
+                                alt="">
+                            <div class="box_4box__whiteopacity"></div>
+                            <div class="box_4box__white"></div>
+                            <div class="box_4box__border"></div>
+                            <a class="box_4box__txt-box flex" href="#">
+                                <div class="flex txt-box__title">
+                                    <span>산림</span>
+                                    <span>Forest</span>
+                                </div>
+                                <div class="campaign__cont__more flex">
+                                    <div class="campaign__cont__more-box">
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="cont__2box flex">
+                            <div class="box_4box">
+                                <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_plastic.jpg"
+                                    alt="">
+                                <div class="box_4box__whiteopacity"></div>
+                                <div class="box_4box__white"></div>
+                                <div class="box_4box__border"></div>
+                                <a class="box_4box__txt-box flex" href="#">
+                                    <div class="flex txt-box__title">
+                                        <span>플라스틱</span>
+                                        <span>Plastic</span>
+                                    </div>
+                                    <div class="campaign__cont__more flex">
+                                        <div class="campaign__cont__more-box">
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="box_4box">
+                                <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/campaign_artic.jpg"
+                                    alt="">
+                                <div class="box_4box__whiteopacity"></div>
+                                <div class="box_4box__white"></div>
+                                <div class="box_4box__border"></div>
+                                <a class="box_4box__txt-box flex" href="#">
+                                    <div class="flex txt-box__title">
+                                        <span>북극</span>
+                                        <span>Artic</span>
+                                    </div>
+                                    <div class="campaign__cont__more flex">
+                                        <div class="campaign__cont__more-box">
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="main-content-2">
+    <div class="con flex">
+        <div class="campaign__title">
+            <a class="titleeng" href="#">Green Project</a>
+            <a class="titleko" href="#">그린피스 주요 프로젝트</a>
+            <div class="greenbar"></div>
+            <div class="content">그린피스는 국내는 물론 전 지구적 환경 문제에 대해 명확한 목표를 가지고 프로젝트를 진행합니다.</div>
+        </div>
+        <div class="campaign__content">
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">Slide 1</div>
+                    <div class="swiper-slide">Slide 2</div>
+                </div>
+                <!-- Add Arrows -->
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </div>
         </div>
     </div>
 </div>
