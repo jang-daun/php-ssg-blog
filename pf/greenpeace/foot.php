58088e1d06556f47b5221b7865e3eda4742e76c8
jang-daun <moone0327@gmail.com>
2023-11-07 12:48:50 +0900

    20231107test

--- pf/greenpeace/foot.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="con flex">
-        <img src="https://cdn.jsdelivr.net/gh/jang-daun/web-resource/green/img/foot.svg" alt="">
-    </div>
-</footer>
-
-</body>
-</html>
--- pf/oworld/foot.php
+++
@@ -1,9 +0,0 @@
-
-<footer class="con-min-width">
-    <img src="https://cdn.jsdelivr.net/gh/jang-daun/oworld/img/footer.svg" alt="">
-</footer>
-
-
-</body>
-
-</html>
